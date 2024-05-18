e45826fa2d9f1658d23589cccb64c837cf8def70
Денис Колесников <73371887@mail.ru>
2024-05-18 16:49:29 +0300

    hw9

---
+++ src/test/java/pro/sky/skyproStreamAPI/SkyproStreamApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.skyproStreamAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyproStreamApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
